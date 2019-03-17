4373b54c51a17603067695b2f3c04f9a3a77ef8e
apisarenko <47703396+apisarenko@users.noreply.github.com>
2019-03-17 06:25:58 +0300

    Update Diplom.py

--- Diplom.py
+++ Diplom.py
@@ -6,7 +6,7 @@ import itertools
 
 TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 params = {
-    'user_ids': 10799607, # При введении любого id код работает по токену Евгения
+    'user_id': 10799607, # При введении любого id код работает
     'access_token': TOKEN,
     'v': 5.92
 }
@@ -16,8 +16,8 @@ user = int(response_get_id.json()['response'][0]['id'])
 
 print(user) # выводит id который вводим в params
 
-response_groups = requests.get('https://api.vk.com/method/groups.get', params) # Выводит группы Евгения независимо от введенного id
-response_friends = requests.get('https://api.vk.com/method/friends.get', params) # Выводит друзей Евгения независимо от введенного id
+response_groups = requests.get('https://api.vk.com/method/groups.get', params) # Выводит группы введенного id
+response_friends = requests.get('https://api.vk.com/method/friends.get', params) # Выводит друзей введенного id
 
 groups_user = response_groups.json()['response']['items']
 friends_id = response_friends.json()['response']['items']
