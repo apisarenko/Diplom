d464c70ca4fd952d7a58e9bb4315153c2f844414
apisarenko <47703396+apisarenko@users.noreply.github.com>
2019-03-24 09:37:09 +0300

    Update Diplom.py

--- Diplom.py
+++ Diplom.py
@@ -1,108 +1,148 @@
 import requests
-from urllib.parse import urlencode
+#from urllib.parse import urlencode
 import itertools
 import json
 import time
 
-TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
-
-user_input = input('Введите id или имя пользователя: ')
-
-try:
-    int(user_input)
-    try:
-        params = {
-            'user_ids': user_input,
-            'access_token': TOKEN,
-            'v': 5.92
-        }
-        response_get_id = requests.get(
-            'https://api.vk.com/method/users.get',
-            params
-        )
-        user_id = int(response_get_id.json()['response'][0]['id'])
-    except KeyError:
-        print('Пользователя с таким id не существует!')
-        exit(0)
-except ValueError:
-    try:
-        params = {
-            'user_ids': user_input,
-            'access_token': TOKEN,
-            'v': 5.92
-        }
-        response_get_id = requests.get(
-            'https://api.vk.com/method/users.get',
-            params
-        )
-        user_id = int(response_get_id.json()['response'][0]['id'])
-    except KeyError:
-        print('Пользователя с таким именем не существует!')
-        exit(0)
-print('id пользователя: ', user_id)
+with open("token.json", 'r') as file:
+    data = json.load(file)
+    TOKEN = data[0]['token']
 
 params = {
-    'user_id': user_id,
     'access_token': TOKEN,
     'v': 5.92
 }
 
-try:
-    response_groups = requests.get('https://api.vk.com/method/groups.get', params)
-    response_friends = requests.get('https://api.vk.com/method/friends.get', params)
-    groups_user = response_groups.json()['response']['items']
-    friends_id = response_friends.json()['response']['items']
-except KeyError:
-    print('Пользователь ограничил доступ к своей странице, '
-          'либо страница заблокирована администрацией ВК,'
-          'получение данных не возможно!!!')
-    exit(0)
-
-print('Группы пользователя: ', groups_user)
-
-print('id друзей пользователя', friends_id)
-
-friend_group_list = []
-for friend in friends_id:
-    params = {
-        'access_token': TOKEN,
-        'user_id': friend,
-        'v': 5.92
-    }
+def check_user(user_input):
     try:
-        friend_groups = requests.get('https://api.vk.com/method/groups.get', params)
-        friend_group_list.append(friend_groups.json()['response']['items'])
-        print('.')
-        time.sleep(1)
-    except Exception as e:
-        print(friend_groups.json())
-general_groups_friend = list(itertools.chain.from_iterable(friend_group_list))
-print(general_groups_friend)
+        int(user_input)
+        try:
+            params['user_ids'] = user_input
+            response_get_id = requests.get(
+                'https://api.vk.com/method/users.get',
+                params
+            )
+            user_id = int(response_get_id.json()['response'][0]['id'])
+        except KeyError:
+            print('Пользователя с таким id не существует!')
+            exit(0)
+    except ValueError:
+        try:
+            params['user_ids'] = user_input
+            response_get_id = requests.get(
+                'https://api.vk.com/method/users.get',
+                params
+            )
+            user_id = int(response_get_id.json()['response'][0]['id'])
+        except KeyError:
+            print('Пользователя с таким именем не существует!')
+            exit(0)
+    return user_id
 
-set_list_of_unique_groups = set(groups_user).difference(set(general_groups_friend))
-list_of_unique_groups = list(set_list_of_unique_groups)
-print(list_of_unique_groups)
-
-output_list = []
-for individual_user_groups in list_of_unique_groups:
-    params_out = {
-        'group_id': individual_user_groups,
-        'fields': 'members_count',
-        'access_token': TOKEN,
-        'v': 5.92
-    }
-
+def friend_list(person_id):
+    params['user_id'] = person_id
+    list_friends_id = []
     try:
-        out_list_of_unique_groups = requests.get('https://api.vk.com/method/groups.getById', params_out)
-        finish_list = out_list_of_unique_groups.json()['response'][0]
-        output_finish_group = {'name': finish_list['name'],
-                       'gid': finish_list['id'],
-                       'members_count': finish_list['members_count']}
-        output_list.append(output_finish_group)
+        response_friends = requests.get('https://api.vk.com/method/friends.get', params)
+        list_friends_id = response_friends.json()['response']['items']
     except KeyError:
-        print(out_list_of_unique_groups.json())
+        print('Пользователь ограничил доступ к своей странице, '
+              'либо страница заблокирована администрацией ВК,'
+              'получение данных не возможно!!!')
+        exit(0)
+    return list_friends_id
 
-print(output_list)
+def group_list_user(person_id):
+    params['user_id'] = person_id
+    list_groups_person = []
+    try:
+        response_groups = requests.get('https://api.vk.com/method/groups.get', params)
+        list_groups_person = response_groups.json()['response']['items']
+    except Exception as e:
+        if list_groups_person.json()['error']['error_code'] == 7:
+            print('Нет прав для выполнения данного действия')
+        if list_groups_person.json()['error']['error_code'] == 18:
+            print('Страница удалена или заблокирована')
+        if list_groups_person.json()['error']['error_code'] == 30:
+            print('Профиль является приватным')
+        if list_groups_person.json()['error']['error_code'] == 260:
+            print('Доступ к списку групп ограничен настройками приватности')
+        exit(0)
+    return list_groups_person
 
-with open('groups.json', 'w', encoding='utf-8') as file:
-    json.dump(output_list, file, ensure_ascii=False)
+def list_groups_friend(friends):
+    list_friends_user = friends
+    friend_group_list_1 = []
+    friend_group_list_2 = []
+    for friend in list_friends_user:
+        params['user_id'] = friend
+        try:
+            friend_groups = requests.get('https://api.vk.com/method/groups.get', params)
+            friend_group_list_1.append(friend_groups.json()['response']['items'])
+            print('.')
+        except Exception as e:
+            time.sleep(0.36)
+            if friend_groups.json()['error']['error_code'] == 6:
+                friend_groups_2 = requests.get('https://api.vk.com/method/groups.get', params)
+                friend_group_list_2.append(friend_groups_2.json()['response']['items'])
+            else:
+                print(friend_groups.json())
+    friend_group_list = friend_group_list_1 + friend_group_list_2
+    return friend_group_list
+
+def unique_groups(group_list, group_user):
+    unique_list = group_list
+    groups_user = group_user
+    general_groups_friend = list(itertools.chain.from_iterable(unique_list))
+    set_list_of_unique_groups = set(groups_user).difference(set(general_groups_friend))
+    list_of_unique_groups = list(set_list_of_unique_groups)
+    return list_of_unique_groups
+
+def json_output(unique_groups):
+    list_of_unique_groups = unique_groups
+    output_list_1 = []
+    output_list_2 = []
+    for individual_user_groups in list_of_unique_groups:
+        params_out = {
+            'group_id': individual_user_groups,
+            'fields': 'members_count'
+        }
+        params.update(params_out)
+        try:
+            out_list_of_unique_groups_1 = requests.get('https://api.vk.com/method/groups.getById', params)
+            finish_list = out_list_of_unique_groups_1.json()['response'][0]
+            output_finish_group_1 = {'name': finish_list['name'],
+                                     'gid': finish_list['id'],
+                                     'members_count': finish_list['members_count']}
+            output_list_1.append(output_finish_group_1)
+        except KeyError:
+            time.sleep(0.34)
+            out_list_of_unique_groups_2 = requests.get('https://api.vk.com/method/groups.getById', params)
+            finish_list = out_list_of_unique_groups_2.json()['response'][0]
+            output_finish_group_2 = {'name': finish_list['name'],
+                                     'gid': finish_list['id'],
+                                     'members_count': finish_list['members_count']}
+            output_list_2.append(output_finish_group_2)
+    output_list = output_list_1 + output_list_2
+    return output_list
+
+def main():
+    user = input('Введите id или имя пользователя: ')
+    user_id = check_user(user)
+    print('id пользователя: ', user_id)
+    list_friends_user = friend_list(user_id)
+    print('Список друзей пользователя: ', list_friends_user)
+    list_groups_user = group_list_user(user_id)
+    print('Список групп пользователя: ', list_groups_user)
+    friend_group_list = list_groups_friend(list_friends_user)
+    print(friend_group_list)
+    list_of_unique_groups = unique_groups(friend_group_list, list_groups_user)
+    print(list_of_unique_groups)
+    output_list = json_output(list_of_unique_groups)
+    print(output_list)
+
+    with open('groups.json', 'w', encoding='utf-8') as json_file:
+        json.dump(output_list, json_file, ensure_ascii=False)
+
+
+main()
